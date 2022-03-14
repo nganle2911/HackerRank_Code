49b0a6d7a616fc73a7f73928ecab1a112a7aba53
Ngan LE <ngan.ltkn2911@gmail.com>
2022-03-14 12:40:01 +0100

    HelloWorld coding

---
+++ Java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Exercise
+ * Input Format: There is no input for this challenge
+ * Output Format: You must print two lines of output: 
+ * 1. Print 'Hello, World.' on the first line
+ * 2. Print 'Hello, Java.' on the second line
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+}
